35f1a9824f242b19c7878322680d4bb35b4064b4
Seth Norris <sethhnorris7@live.com>
2017-01-23 19:12:12 -0700

    Basic treeNode

---
+++ src/com/teamhandsome/compiler/models/SyntaxType.java
@@ -0,0 +1,5 @@
+package com.teamhandsome.compiler.models;
+
+public enum SyntaxType {
+	
+}
---
+++ src/com/teamhandsome/compiler/models/TreeNode.java
@@ -0,0 +1,12 @@
+package com.teamhandsome.compiler.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+	
+	private List<TreeNode> children = new ArrayList<TreeNode>();
+	
+	
+
+}
