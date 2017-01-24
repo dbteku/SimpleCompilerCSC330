9e3b5ee4e5206280c2aecc485c1f6c2e42fdb5c9
= <dbteku@hotmail.com>
2017-01-24 09:28:38 -0700

    always initialize in the constructor SETH

--- src/com/teamhandsome/compiler/models/TreeNode.java
+++ src/com/teamhandsome/compiler/models/TreeNode.java
@@ -2,11 +2,15 @@ package com.teamhandsome.compiler.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TreeNode {
-	
-	private List<TreeNode> children = new ArrayList<TreeNode>();
-	
-	
+
+	private List<TreeNode> children;
+
+	public TreeNode() {
+		children = new ArrayList<>();
+	}
 
 }
