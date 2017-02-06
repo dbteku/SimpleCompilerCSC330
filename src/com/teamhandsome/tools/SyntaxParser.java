aaec746090430dcdd2b70fce2d4acb70dd37887e
dbteku <dbteku@hotmail.com>
2017-02-06 00:21:36 -0700

    MathOpToVariable

--- src/com/teamhandsome/tools/SyntaxParser.java
+++ src/com/teamhandsome/tools/SyntaxParser.java
@@ -17,7 +17,9 @@ import com.teamhandsome.compiler.syntax.rules.If;
 import com.teamhandsome.compiler.syntax.rules.IfElse;
 import com.teamhandsome.compiler.syntax.rules.IfStatement;
 import com.teamhandsome.compiler.syntax.rules.IncrementorToModifiedAssignment;
+import com.teamhandsome.compiler.syntax.rules.MathOpToNestedMathOp;
 import com.teamhandsome.compiler.syntax.rules.MathOpVariableName;
+import com.teamhandsome.compiler.syntax.rules.MathOperation;
 import com.teamhandsome.compiler.syntax.rules.MathOperationParam;
 import com.teamhandsome.compiler.syntax.rules.MathOperationStatement;
 import com.teamhandsome.compiler.syntax.rules.MathOperationToBinaryComparison;
@@ -27,9 +29,11 @@ import com.teamhandsome.compiler.syntax.rules.MethodCallToMathOperation;
 import com.teamhandsome.compiler.syntax.rules.ModifiedAssignmentStatement;
 import com.teamhandsome.compiler.syntax.rules.MultiDeclareVariable;
 import com.teamhandsome.compiler.syntax.rules.NameParToMath;
+import com.teamhandsome.compiler.syntax.rules.NameToMathOperation;
 import com.teamhandsome.compiler.syntax.rules.ReturnStatement;
 import com.teamhandsome.compiler.syntax.rules.StatementToBody;
 import com.teamhandsome.compiler.syntax.rules.StatementToNestedStatement;
+import com.teamhandsome.compiler.syntax.rules.TypeToTypeArray;
 import com.teamhandsome.compiler.syntax.rules.TypeVariable;
 import com.teamhandsome.compiler.syntax.rules.TypedMathOperationBodyToFunction;
 import com.teamhandsome.compiler.syntax.rules.TypedVariableName;
@@ -66,7 +70,8 @@ public class SyntaxParser {
 				new WhileLoopStatement(), new IfStatement(), new MathOperationStatement(), new ModifiedAssignmentStatement(),
 				new MultiDeclareVariable(), new TypeVariable(), new MathVariable(), new TypedVariableName(),
 				new MultiDeclareVariable(), new MathOpVariableName(), new BodyToForLoop(), new If(), new IfElse(),
-				new WhileLoop(), new NameParToMath(), new ValueToMathOp()});
+				new WhileLoop(), new NameParToMath(), new ValueToMathOp(), new MathOperation(), new MathOpToNestedMathOp(),
+				new NameToMathOperation(), new TypeToTypeArray()});
 	}
 
 	public SyntaxTree toTree(List<Token> tokens){
