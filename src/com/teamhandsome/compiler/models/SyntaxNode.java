799c7519951d6132f96f58db4af680b9d8c6fe08
dbteku <dbteku@hotmail.com>
2017-02-06 03:13:18 -0700

    Tree ToString Done

--- src/com/teamhandsome/compiler/models/SyntaxNode.java
+++ src/com/teamhandsome/compiler/models/SyntaxNode.java
@@ -8,11 +8,11 @@ public class SyntaxNode {
 	private List<SyntaxNode> children;
 	private NodeType type;
 	private Token token;
-	
+
 	public SyntaxNode(NodeType type) {
 		this(type, null);
 	}
-	
+
 	public SyntaxNode(NodeType type, Token token){
 		children = new ArrayList<>();
 		this.type = type;
@@ -24,7 +24,7 @@ public class SyntaxNode {
 			this.children.add(node);
 		}
 	}
-	
+
 	public Token getToken() {
 		Token token = new Token("", TokenType.SPACE);
 		if(this.token != null){
@@ -32,18 +32,43 @@ public class SyntaxNode {
 		}
 		return token;
 	}
-	
+
 	public NodeType getType() {
 		return type;
 	}
-	
+
 	public boolean isNull(){
 		return type == NodeType.NULL;
 	}
-	
+
 	@Override
 	public String toString() {
-		return type.toString();
+		return toString(0);
 	}
-	
+
+	private String toString(int indentLevel){
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < indentLevel; i++)
+		{
+			builder.append("..");
+		}
+
+		builder.append(type.toString());
+		builder.append(":");
+
+		if (children.size() == 0){
+			builder.append(token.toString());
+			builder.append("\n");
+		}
+		else{
+			builder.append("\n");
+		}
+		for (int i = 0; i < children.size(); i++)
+		{
+			builder.append(children.get(i).toString(indentLevel + 1));
+		}
+
+		return builder.toString();
+	}
+
 }
