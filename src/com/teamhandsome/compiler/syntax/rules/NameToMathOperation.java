1710b2322a57f593666a4d06756db4f1b0ae8a71
KurtisLloyd <kurtyboyfloyd@gmail.com>
2017-02-06 00:12:29 -0700

    NameToMathOperation

---
+++ src/com/teamhandsome/compiler/syntax/rules/NameToMathOperation.java
@@ -0,0 +1,32 @@
+package com.teamhandsome.compiler.syntax.rules;
+
+import java.util.List;
+
+import com.teamhandsome.compiler.models.NodeType;
+import com.teamhandsome.compiler.models.SyntaxNode;
+
+public class NameToMathOperation extends SyntaxRule{
+
+	private static final int SIZE = 1;
+	
+	public NameToMathOperation() {
+		
+	}
+	
+	@Override
+	public boolean isRule(List<SyntaxNode> nodes, SyntaxNode next) {
+		return nodes.size() >= 2 
+				&& nodes.get(nodes.size() - 1).getType() == NodeType.NAME 
+				&& !next.getToken().getValue().equals("(") 
+				&& !(nodes.get(nodes.size() - 2).getType() == NodeType.SYMBOL
+				&& nodes.get(nodes.size() - 2).getToken().getValue().equals(",")) 
+				&& next.getType() != NodeType.INCREMENTOR 
+				&& next.getType() != NodeType.EQUALS;
+	}
+
+	@Override
+	public SyntaxNode applyRule(List<SyntaxNode> nodes) {
+		return constructNode(NodeType.MATH_OPERATION, SIZE, nodes);
+	}
+
+}
