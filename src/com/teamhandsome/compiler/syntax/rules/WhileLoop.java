1ab973740cd280959ea590a337af2d7767c6781f
dbteku <dbteku@hotmail.com>
2017-02-05 23:51:23 -0700

    WhileLoop Done

---
+++ src/com/teamhandsome/compiler/syntax/rules/WhileLoop.java
@@ -0,0 +1,34 @@
+package com.teamhandsome.compiler.syntax.rules;
+
+import java.util.List;
+
+import com.teamhandsome.compiler.models.NodeType;
+import com.teamhandsome.compiler.models.SyntaxNode;
+
+public class WhileLoop extends SyntaxRule{
+
+	private static final int SIZE = 5;
+
+	public WhileLoop() {
+
+	}
+
+	@Override
+	public boolean isRule(List<SyntaxNode> nodes, SyntaxNode next) {
+		return nodes.size() >= 5 
+				&& nodes.get(nodes.size() - 1).getType() == NodeType.BODY 
+				&& nodes.get(nodes.size() - 2).getType() == NodeType.SYMBOL
+				&& nodes.get(nodes.size() - 2).getToken().getValue().equals(")") 
+				&& nodes.get(nodes.size() - 3).getType()== NodeType.BINARY_COMPARATOR 
+				&& nodes.get(nodes.size() - 4).getType() == NodeType.SYMBOL 
+				&& nodes.get(nodes.size() - 4).getToken().getValue().equals("(") 
+				&& nodes.get(nodes.size() - 5).getType() == NodeType.KEYWORD 
+				&& nodes.get(nodes.size() - 5).getToken().getValue().equals("while");
+	}
+
+	@Override
+	public SyntaxNode applyRule(List<SyntaxNode> nodes) {
+		return constructNode(NodeType.WHILELOOP, SIZE, nodes);
+	}
+
+}
